e013cdb20e753bef8eae3bc72b29be0ef59088dc
Hawar-Dzaee <hawardizayee@gmail.com>
2026-04-26 11:46:52 -0400

    introducing engine

---
+++ examples/my_image_jepa/engine.py
@@ -0,0 +1,28 @@
+import torch
+import tqdm 
+
+def train_epoch(
+        model,
+        train_loader,
+        optimizer,
+        scheduler,
+        linear_probe,
+        scaler,
+        device,
+        epoch,
+        loss_fn,
+        use_amp=True,
+        dtype=torch.float16,
+        tqdm_silent=False   # ?
+):
+    """Train for one epoch."""
+    model.train()
+    linear_probe.train()
+
+    # Dynamic loss accumulator 
+    loss_totals = {}
+    total_linear_loss = 0 
+    linear_correct = 0 
+    linear_total = 0 
+
+    pbar = tqdm(train_loader,desc=f"Epoch {epoch}",disable=tqdm_silent)
---
+++ examples/my_image_jepa/main.py
@@ -0,0 +1,4 @@
+
+from model import ResNet18,ImageSSL
+from lars import LARS
+from scheduler import WarmupCosineScheduler
