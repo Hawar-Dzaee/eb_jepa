cd3dadfd924510433ae0d6108c355c602ccdaf26
Hawar-Dzaee <hawardizayee@gmail.com>
2026-04-28 17:36:12 -0400

    adding default config

--- examples/my_image_jepa/main.py
+++ examples/my_image_jepa/main.py
@@ -2,3 +2,12 @@
 from model import ResNet18,ImageSSL
 from lars import LARS
 from scheduler import WarmupCosineScheduler
+
+
+def run(
+        fnmae: str = "examples/my_image_jepa/default.yaml",
+        cfg = None,
+        folder = None,
+        **overrides
+):
+    pass 
